31a6321fd6b6f8a01899e27f49ed15490cfe8099
William Hogman <william@hogman.me>
2011-09-22 22:11:38 +0200

    added diag system for data

---
+++ fsbbs/data/__main__.py
@@ -0,0 +1,5 @@
+from .diag import runDiag
+
+
+print("running diag for the dataapi")
+runDiag()
---
+++ fsbbs/data/diag.py
@@ -0,0 +1,10 @@
+#!/bin/env python2
+from .model import Container
+import datasource
+
+def runDiag():
+    pass
+        
+
+
+    
