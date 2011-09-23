f98e7eafdca2fbdbfe367d09f0560ff40c55be5b
William Hogman <william@hogman.me>
2011-09-23 21:55:47 +0200

    added a service class for interfacing with the data

---
+++ fsbbs/service.py
@@ -0,0 +1,31 @@
+"""
+module providing access to the bbs
+"""
+from .data import model
+from .data import datasource
+from twisted.internet import defer
+
+class BBSService(object):
+
+    def __init__(self,ds):
+        self.ds = ds
+
+    def _msg(self,msg,kind="msg"):
+        return dict(msg=msg,kind=kind)
+
+    @defer.inlineCallbacks
+    def getFrontpage(self):
+
+        try:
+            mp = model.Container(6,self.ds)
+            yield mp.ready
+        except model.ThingNotFoundError:
+            defer.returnValue({
+                    "msg": self._msg("Couldn't find any content for the front page","error")
+                    })
+        
+        defer.returnValue({
+                "main": mp
+                })
+
+service = BBSService(datasource.getDatasource())
