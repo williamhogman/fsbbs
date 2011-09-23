a58d3d84ba5fff7446619570089da5d0ffcd637b
William Hogman <william@hogman.me>
2011-09-23 18:16:43 +0200

    added diagnostics for data

--- fsbbs/data/__main__.py
+++ fsbbs/data/__main__.py
@@ -1,5 +1,13 @@
-from .diag import runDiag
+from .diag import DiagFactory
+import datasource
+from twisted.internet import reactor
+
+print("running diag for the fsbbs.data")
+print("be careful with this it allows db access to EVERYONE")
+print("seriously...")
+
+reactor.listenTCP(8734,DiagFactory(datasource.getDatasource()))
+reactor.run()
 
 
-print("running diag for the dataapi")
-runDiag()
+
