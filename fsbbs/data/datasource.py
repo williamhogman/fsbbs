0ab5018bf90b7950f72cf00ed8b58ca0db6752af
William Hogman <william@hogman.me>
2011-09-23 21:57:25 +0200

    fixed zrange command

--- fsbbs/data/datasource.py
+++ fsbbs/data/datasource.py
@@ -26,8 +26,8 @@ class DataSource:
     def incr(self,key,am=1):
         return self.api.incr(key,am)
 
-    def zrange(self,key,start=0,stop=-1,withScore=False):
-        return self.api.zrange(key,start,stop,withScore)
+    def zrange(self,key,start=0,stop=-1):
+        return self.api.zrange(key,start,stop)
     
     def zadd(self,key,score,value):
         return self.api.zadd(key,score,value)
