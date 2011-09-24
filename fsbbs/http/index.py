981c6d1db475e3e3c62a256d36c6b7bc04b54e3d
William Hogman <william@hogman.me>
2011-09-24 20:58:07 +0200

    added session support to the index handler

--- fsbbs/http/index.py
+++ fsbbs/http/index.py
@@ -1,13 +1,18 @@
 import cyclone.web
-from .handler import BaseHandler,SimpleJSON,SimpleMsgpack
+from .handler import BaseHandler,SimpleJSON,SimpleMsgpack,SessionAuthMixin
 from twisted.internet import defer
 from ..service import service
 from ..output import json_out,msgpack_out
 
-class IndexHandler(BaseHandler):
+class IndexHandler(BaseHandler,SessionAuthMixin):
     @defer.inlineCallbacks
     def get(self):
-        fp = yield service.getFrontpage()        
+        ses_ver = self.verifySession()
+        
+        fp = yield service.getFrontpage()
+        
+        if (yield ses_ver):
+            self.write("welcome back")
         if "msg" in fp:
             self.write(fp['msg']['msg'])
 
