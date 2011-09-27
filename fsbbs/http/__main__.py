7a7c67d5c92e30f2c98ac21b46a1ba26eeb6f765
William Högman <william@hogman.me>
2011-09-27 16:28:39 +0200

    added twisted logging stdout for http module

--- fsbbs/http/__main__.py
+++ fsbbs/http/__main__.py
@@ -1,6 +1,9 @@
 from twisted.internet import defer, reactor
+from twisted.python import log
 from .application import Application
+import sys
 
+log.startLogging(sys.stdout)
 app = Application()
 reactor.listenTCP(8888, app)
 reactor.run()
