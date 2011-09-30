983b80373702adc004a8929d8da77965db018012
William Högman <william@hogman.me>
2011-09-30 16:35:47 +0200

    added a basic dom traversing data-id finder, user pages are still NYI

--- themes/default/static/app.js
+++ themes/default/static/app.js
@@ -1,5 +1,22 @@
 $script("http://cdnjs.cloudflare.com/ajax/libs/prototype/1.7.0.0/prototype.js","ptype");
 $script.ready("ptype",
 	      function(){
+		  api = window.fsbbs = {};
+
+		  forumLinkClicked = function(){
+		      var cur = this;
+		      var id = cur.readAttribute("data-id")
+		      while(id === null)
+			  {
+			      cur = cur.up();
+			      id = cur.readAttribute("data-id");
+			  }
+		      alert(id);
+		      return true;
+		  };
+		  
+		  $$("article[data-id] a").each(function(item){
+						    item.observe("click",forumLinkClicked);
+						});
 		  
 	      });
