3b77923476c7a53654d4bcf34f7cf47f0d265374
William Högman <william@hogman.me>
2011-10-13 17:41:42 +0200

    moved the frontpage to thing one

--- fsbbs/service.py
+++ fsbbs/service.py
@@ -5,7 +5,9 @@ from .data import datasource,model
 from twisted.internet import defer
 
 class BBSService(object):
-
+    """
+    Service providing access to the BBS data
+    """
     def __init__(self,ds):
         self.ds = ds
         self._basicInfo = None
@@ -15,9 +17,9 @@ class BBSService(object):
 
     @defer.inlineCallbacks
     def getBasicInfo(self):
-        """ gets basic info about a request """
+        """ gets basic information about the hosted forum"""
         if self._basicInfo is None:
-            mp = model.Forum(6,self.ds)
+            mp = model.Forum(1,self.ds)
             yield mp.ready
             self._basicInfo = {"forum": (yield mp.asDict(minimal=True))}
 
@@ -26,8 +28,11 @@ class BBSService(object):
 
     @defer.inlineCallbacks 
     def getFrontpage(self):
+        """
+        Gets the frontpage
+        """
         try:
-            mp = model.Forum(6,self.ds)
+            mp = model.Forum(1,self.ds)
             yield mp.ready
         except model.ThingNotFoundError:
             defer.returnValue({
@@ -40,6 +45,9 @@ class BBSService(object):
 
     @defer.inlineCallbacks
     def getThing(self,tid,throw=False):
+        """ 
+        Gets a thing from the database'
+        """
         try:
             thing = yield model.anythingFromId(tid,self.ds,ready=True)
         except model.ThingNotFoundError:
@@ -50,6 +58,9 @@ class BBSService(object):
             
     @defer.inlineCallbacks
     def postToThing(self,tid,text,user):
+        """
+        Create a post inside a thing
+        """
         cont = yield model.anythingFromId(tid,self.ds,ready=True)
         post = model.Post.new(text,user.uid,ds=self.ds)
         if not hasattr(cont,"add"):
