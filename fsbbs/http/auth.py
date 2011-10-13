669f14ac06a7d7fd9a17e753dd9ee5abfe9ec8df
William Högman <william@hogman.me>
2011-10-13 21:36:52 +0200

    did pylint for auth.py

--- fsbbs/http/auth.py
+++ fsbbs/http/auth.py
@@ -1,12 +1,13 @@
-import cyclone.web
-from .handler import BaseHandler,SimpleJSON,SimpleMsgpack,SessionAuthMixin
+"""
+Module for http handlers involved in authentication.
+"""
+from .handler import BaseHandler, SessionAuthMixin
 from twisted.internet import defer
 from ..service import service
-from ..output import json_out,msgpack_out,html
-from ..a3 import AuthService,User
-from ..data import datasource
+from ..output import json_out, html
+from ..a3 import AuthService
 
-class LoginHandler(BaseHandler,SessionAuthMixin):
+class LoginHandler(BaseHandler, SessionAuthMixin):
     """provides a login form"""
     @defer.inlineCallbacks
     def get(self):
@@ -14,8 +15,8 @@ class LoginHandler(BaseHandler,SessionAuthMixin):
         if logged_in:
             self.redirect("/index.html")
             return
-        dt = yield service.getBasicInfo()
-        html.OutputFormatter.dump("login.html",dt,self)
+        data = yield service.getBasicInfo()
+        html.OutputFormatter.dump("login.html", data, self)
             
     @defer.inlineCallbacks
     def post(self):
@@ -27,16 +28,17 @@ class LoginHandler(BaseHandler,SessionAuthMixin):
         username = self.get_argument("username")
         password = self.get_argument("password")
         auth = AuthService()
-        res = yield auth.getChain("default").run({"username": username,"password": password})
+        res = yield auth.getChain("default").run({"username": username,
+                                                  "password": password})
 
         if res.success:
             # if we've been asked to store a secret
             if 'set_session_secret' in res:
-                self.set_cookie("s",res['set_session_secret'])
+                self.set_cookie("s", res['set_session_secret'])
             self.redirect("/index.html")
         else:
-            
-            auth = {"logged_in": False, "msg": {"kind": "error", "msg": "Login failed"}}
+            auth = {"logged_in": False, "msg": {"kind": "error",
+                                                "msg": "Login failed"}}
             auth.update((yield service.getBasicInfo()))
             html.OutputFormatter.dump("login.html",
                                       auth,
@@ -46,7 +48,7 @@ class LoginHandler(BaseHandler,SessionAuthMixin):
 
 
 
-class RegisterHandler(BaseHandler,SessionAuthMixin):
+class RegisterHandler(BaseHandler, SessionAuthMixin):
     """ provides a register form """
     @defer.inlineCallbacks
     def get(self):
@@ -54,8 +56,8 @@ class RegisterHandler(BaseHandler,SessionAuthMixin):
         if logged_in:
             self.redirect("/index.html")
             return
-        dt = yield service.getBasicInfo()
-        html.OutputFormatter.dump("register.html",dt,self)
+        data = yield service.getBasicInfo()
+        html.OutputFormatter.dump("register.html", data, self)
     @defer.inlineCallbacks
     def post(self):
         logged_in = yield self.verifySession()
@@ -65,19 +67,20 @@ class RegisterHandler(BaseHandler,SessionAuthMixin):
         username = self.get_argument("username")
         password = self.get_argument("password")
         auth = AuthService()  
-        res = yield auth.getChain("register").run({"username":username,"new_password": password})
+        res = yield auth.getChain("register").run({"username":username,
+                                                   "new_password": password})
         if res.success:
             if 'set_session_secret' in res:
-                self.set_cookie("s",res['set_session_secret'])
+                self.set_cookie("s", res['set_session_secret'])
             data = dict()
             data.update((yield service.getBasicInfo()))
             data['new_username'] = username.lower()
             data['hidelogin'] = True
-            html.OutputFormatter.dump("new_user.html",data,self)
+            html.OutputFormatter.dump("new_user.html", data, self)
         else:
-            dt = {"msg": {"msg": "Could not register user", "kind": "error"}}
-            dt.update((yield service.getBasicInfo()))
-            html.OutputFormatter.dump("register.html",dt,self)
+            data = {"msg": {"msg": "Could not register user", "kind": "error"}}
+            data.update((yield service.getBasicInfo()))
+            html.OutputFormatter.dump("register.html", data, self)
 
 
 class LogoutHandler(BaseHandler):
@@ -89,7 +92,7 @@ class LogoutHandler(BaseHandler):
 
 from ..output import json_out
 
-class RegisterJSONHandler(BaseHandler,SessionAuthMixin):
+class RegisterJSONHandler(BaseHandler, SessionAuthMixin):
     """
     provides a registration method callable via JSON+XHR
     """
@@ -97,28 +100,32 @@ class RegisterJSONHandler(BaseHandler,SessionAuthMixin):
     def post(self):
         logged_in = yield self.verifySession()
         if logged_in:
-            self.finish(json_out.serialize({"status": "invalid","uid": self.user.uid}))
+            self.finish(json_out.serialize({"status": "invalid",
+                                            "uid": self.user.uid}))
             return
         
         username = self.get_argument("username")
         password = self.get_argument("password")
         
         auth = AuthService()
-        res = yield auth.getChain("register").run({"username": username,"new_password": password})
+        res = yield auth.getChain("register").run({"username": username,
+                                                   "new_password": password})
 
         self.set_header("Content-Type","application/json")
         if res.success:
             if 'set_session_secret' in res:
-                self.set_cookie("s",res['set_session_secret'])
+                self.set_cookie("s", res['set_session_secret'])
 
-            self.finish(json_out.serialize({"status": "success", "uid": res.uid, "username": username.lower()}))
+            self.finish(json_out.serialize({"status": "success",
+                                            "uid": res.uid,
+                                            "username": username.lower()}))
         else:
             self.finish(json_out.serialize({"status": "failure"}))
             return
                         
         
 
-class LoginJSONHandler(BaseHandler,SessionAuthMixin):
+class LoginJSONHandler(BaseHandler, SessionAuthMixin):
     """
     provides a login method callable via JSON+XHR
     """
@@ -126,22 +133,27 @@ class LoginJSONHandler(BaseHandler,SessionAuthMixin):
     def post(self):
         logged_in = yield self.verifySession()
         if logged_in:
-            self.finish(json_out.serialize({"status": "invalid","uid": self.user.uid}))
+            self.finish(json_out.serialize({"status": "invalid",
+                                            "uid": self.user.uid}))
             return
         username = self.get_argument("username")
         password = self.get_argument("password")
         auth = AuthService()
-        res = yield auth.getChain("default").run({"username": username,"password": password})
+        res = yield auth.getChain("default").run({"username": username,
+                                                  "password": password})
 
-        self.set_header("Content-Type","application/json")
+        self.set_header("Content-Type", "application/json")
         if res.success:
             # if we've been asked to store a secret
             if 'set_session_secret' in res:
-                self.set_cookie("s",res['set_session_secret'])
+                self.set_cookie("s", res['set_session_secret'])
 
-            self.finish(json_out.serialize({"status": "success","msg": "Logged in", "uid": res.uid}))
+            self.finish(json_out.serialize({"status": "success",
+                                            "msg": "Logged in",
+                                            "uid": res.uid}))
         else:
-            self.finish(json_out.serialize({"status": "failure","msg": "Incorrect credentials"}))
+            self.finish(json_out.serialize({"status": "failure",
+                                            "msg": "Incorrect credentials"}))
         
             
 
@@ -157,9 +169,9 @@ class LogoutJSONHandler(BaseHandler):
         
 import application
 
-application.addHandler(r"/api/logout.json",LogoutJSONHandler)
-application.addHandler(r"/api/login.json",LoginJSONHandler)
-application.addHandler(r"/api/register.json",RegisterJSONHandler)
-application.addHandler(r"/login.html",LoginHandler)
-application.addHandler(r"/logout.html",LogoutHandler)
-application.addHandler(r"/register.html",RegisterHandler)
+application.addHandler(r"/api/logout.json", LogoutJSONHandler)
+application.addHandler(r"/api/login.json", LoginJSONHandler)
+application.addHandler(r"/api/register.json", RegisterJSONHandler)
+application.addHandler(r"/login.html", LoginHandler)
+application.addHandler(r"/logout.html", LogoutHandler)
+application.addHandler(r"/register.html", RegisterHandler)
