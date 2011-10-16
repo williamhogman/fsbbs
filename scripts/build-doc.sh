477e0e38d3014fe2012efb7ea3e422ebb2f3d3aa
William Hogman <william@hogman.me>
2011-10-16 15:42:31 +0200

    improved doc build script

--- scripts/build-doc.sh
+++ scripts/build-doc.sh
@@ -5,17 +5,32 @@ mkdir .bld
 
 cat *.md > .bld/markdown_all.md
 
+echo "Copying html and css"
 cp head.html .bld/head.html
 cp foot.html .bld/foot.html
 cp style.css .bld/style.css
-cp -r img/ .bld/img
+
+
+echo "Converting dia to svg"
+find ./img/ -name "*.dia" | parallel dia -e {.}.svg {}
+
+mkdir .bld/img
+cp -v img/*.png .bld/img
+cp -v img/*.svg .bld/img
+
+
 
 cd .bld
 
+echo "Generating markdown"
 markdown markdown_all.md > markdown_all.html
 
+echo "Generating HTML"
 cat head.html markdown_all.html foot.html > all.html
 
+echo "Printing HTML"
 wkhtmltopdf all.html all.pdf
 
+echo "Copying result"
 cp all.pdf ../
+echo "DONE"
