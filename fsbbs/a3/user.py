dbee476187340c7ce3a6329ee40c3dde3f25796e
William Hogman <william@hogman.me>
2011-12-03 00:45:34 +0100

    User now uses redis hashes for storage
    Redis hashes trade some CPU in exchange for lower memory usage, based
    on that O(N) is similar to O(1) for a small N

--- fsbbs/a3/user.py
+++ fsbbs/a3/user.py
@@ -1,30 +1,50 @@
+from functools import partial
 from twisted.internet import defer
 
 class User(object):
     """ class representing the basic information stored about a user"""
+    __hash_keys = ['username','email']
     def _key(self,name):
         return "user:{}:{}".format(self.uid,name)
     @property
     def _hash(self):
         return "user:{}".format(self.uid)
+
     def _hget(self,name):
         return self.ds.hget(self._hash,name)
 
+    def _hmget(self,*keys):
+        return self.ds.hmget(self._hash,*keys)
+
     def __init__(self,uid,datasource):
+        self.ds = datasource
         if uid > 0:
             self.uid = uid
-            self.ready = self.load(datasource)
-
-
+            self.ready = self.load()
     
+
     @defer.inlineCallbacks
-    def load(self,ds):
-        self.ds = ds
-        self.username = yield self._hget("username")
+    def load(self):
+        main = self._hmget(*self.__hash_keys)
+        main.addCallback(partial(zip,self.__hash_keys))
+        main.addCallback(partial(map,lambda (k,v): setattr(self,k,v)))
+        yield main
         
 
+
+
     @classmethod
-    def withProperty(cls,datasource,prop):
+    @defer.inlineCallbacks
+    def by_ids(cls,uids,ds,ready=False):
+        users = list()
+        for uid in uids:
+            users.append(cls(uid,ds))
+        if ready:
+            yield defer.DeferredList([user.ready for user in users])
+        defer.returnValue(users)
+
+    @classmethod
+    def with_property(cls,datasource,prop):
         """ 
         returns a redis set (without actually getting it) with
         users having a certain property
