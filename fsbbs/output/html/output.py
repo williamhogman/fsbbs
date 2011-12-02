af8293fee796db0dc886d13cc619f2dce4b7f695
William Hogman <william@hogman.me>
2011-12-02 12:26:56 +0100

    corrected includes in output/html/output.py

--- fsbbs/output/html/output.py
+++ fsbbs/output/html/output.py
@@ -1,6 +1,5 @@
-from jinja2 import Environment, FileSystemLoader
 from datetime import datetime,date
-from ...service import service
+from jinja2 import Environment, FileSystemLoader
 import markdown
 
 def markdownFilter(text):
