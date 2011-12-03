8aa6b7daf0c2af0d9bf4ffe431d2265583ab68fc
William Hogman <william@hogman.me>
2011-12-03 01:58:08 +0100

    html/output.py now uses the correct api

--- fsbbs/output/html/output.py
+++ fsbbs/output/html/output.py
@@ -1,53 +1,12 @@
-from datetime import datetime,date
-from jinja2 import Environment, FileSystemLoader
-from functools import partial
+from ..template import TemplateEngine
 
-try:
-    import misaka
-except ImportError:
-    import markdown
-    _markdown = partial(markdown.markdown,safe_mode=True)
-else:
-    _markdown = partial(misaka.html,render_flags=misaka.HTML_SKIP_HTML)
-
-
-markdownFilter  = _markdown
-
-def dateFilter(dt):
-    """ a human readable date"""
-    # TODO: expand on this to include timedeltas and other things.
-    if dt.date() == date.today():
-        return dt.strftime("Today %H:%M")
-    elif dt.year != datetime.now().year:
-        return dt.strftime("%a, %d. %b %Y %H:%M")
-    else:
-        return dt.strftime("%a, %d. %b %H:%M")
-
-class HTMLOutputFormatter:
+class HTMLOutputFormatter(TemplateEngine):
     """ processes dict objects and uses a template to format them as HTML """
 
     def __init__(self):
         """Creates a new instance of HTMLOutputFormatter"""
         # actually we don't support themes yet :o
-        self.env = Environment(loader=FileSystemLoader('themes/default/'))
-        self.env.filters['markdown'] = markdownFilter
-        self.env.filters['nicedate'] = dateFilter
-                         
-
-    def render(self,name,data):
-        """ 
-        returns an object with a dump function that can be called on a file-like object
-        """
-        return self._getTemplate(name).stream(data)
-
-    def dump(self,name,data,fp):
-        """ writes a rendered template to a file for output """
-        self._getTemplate(name).stream(data).dump(fp)
-
-
-    def _getTemplate(self,name):
-        """ gets a template from the jinja2 backend """
-        return self.env.get_template(name)
+        TemplateEngine.__init__(self,"themes/default")
 
 
 OutputFormatter = HTMLOutputFormatter()
