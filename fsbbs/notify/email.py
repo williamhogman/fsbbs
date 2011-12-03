5f3cb3466c9cf0b53486cc9479c937a614532abc
William Hogman <william@hogman.me>
2011-12-03 04:06:09 +0100

    adapted notify/email to use the email sending api

--- fsbbs/notify/email.py
+++ fsbbs/notify/email.py
@@ -2,30 +2,39 @@
 Notifications via email
 """
 from twisted.internet import defer
+from twisted.python import log
 from zope.interface import implements
 from interface import INotificationService
 from ..data import datasource 
 from ..mail import outgoing
 from ..a3.user import User
+from fsbbs.output import mail
 
 class EmailNotificationService(object):
     implements(INotificationService)
     @defer.inlineCallbacks
     def process(self,notf):
         ds = datasource.getDatasource()
+        log.msg("email notif")
         email_to = notf.recp.intersect(User.with_property(ds,"notify_email"))
         
-        body = notf.kind+"\n"+str(notf.obj) # todo: actual templates ;-)
+        body = self.__build_body(notf)
 
         email_to = yield email_to
         users = yield User.by_ids(email_to,ds)
 
         for user in users:
             yield user.ready
+
+
             if user.email is None:
                 continue
             outgoing.NotificationMessage(to=user.email,sender="list",body=body).send()
+
+
         
         
         
         
+    def __build_body(self,notf):
+        return mail.Output.render_plain_body(notf.kind,notf.obj)
