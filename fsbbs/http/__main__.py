f28c96e29f1374ea70faae3b5ea64bd4b6900e14
William Hogman <william@hogman.me>
2011-12-06 01:40:06 +0100

    deleted obsolete __main__ files in the http server and in the mail server

--- fsbbs/http/__main__.py
+++
@@ -1,9 +0,0 @@
-from twisted.internet import defer, reactor
-from twisted.python import log
-from .application import Application
-import sys
-
-log.startLogging(sys.stdout)
-app = Application()
-reactor.listenTCP(3037, app)
-reactor.run()
